634fd4dbdba07332ef05e0d08611dda114676c55
JoshuaJoji <joshuajoji@outlook.com>
2025-10-21 14:14:35 +0100

    Updated duration_calculator.py and moved random_dates.csv

--- 20_10_25/duration_calculator.py
+++ 20_10_25/duration_calculator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def date_diff_calculator(user_date):
     today = np.datetime64('today')
@@ -9,7 +10,16 @@ def date_diff_calculator(user_date):
     day_diff = (input_date - today).astype(int)
     return day_diff
 
+def calculate_csv(file_name="random_dates.csv"):
+    df = pd.read_csv(file_name, header=None, names=["date"])
+    df['Days_Difference'] = df['date'].apply(date_diff_calculator)
+    print(df)
+    df.to_csv("random_dates_with_differences.csv", index=False)
+    print("CSV file date differences saved as 'random_dates_with_differences.csv'.")
+    return df
+
 if __name__ == "__main__":
     user_date = input("Enter a date (YYYY-MM-DD): ")
     day_diff = date_diff_calculator(user_date)
     print(f"The difference in days is: {day_diff} days")
+    calculate_csv("random_dates.csv")
